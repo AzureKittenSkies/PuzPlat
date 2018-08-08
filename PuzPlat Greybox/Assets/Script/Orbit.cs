a24f4076657902192dc071bc23558f54170e9e83
AzureKittenSkies <azzhayman@gmail.com>
2018-08-08 17:38:59 +1000

    More work, if this doesn't actually work on 2018, revert to previous commit

---
+++ PuzPlat Greybox/Assets/Script/Orbit.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 5.0f;
+    public float xSpeed = 120.0f;
+    public float ySpeed = 120.0f;
+
+    public float yMinLimit = -20f;
+    public float yMaxLimit = 80f;
+
+    public float distanceMin = .5f;
+    public float distanceMax = 15f;
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.y;
+        y = angles.x;
+
+        transform.SetParent(null);
+    }
+
+    void LateUpdate()
+    {
+        if (target)
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            float scrollWheel = Input.GetAxis("Mouse ScrollWheel");
+            distance = Mathf.Clamp(distance - scrollWheel * 5, distanceMin, distanceMax);
+
+            //RaycastHit hit;
+            //if (Physics.Linecast(target.position, transform.position, out hit))
+            //{
+            //    distance -= hit.distance;
+            //}
+
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            Vector3 position = rotation * negDistance + target.position;
+
+            transform.rotation = rotation;
+            transform.position = position;
+
+            Vector3 euler = transform.eulerAngles;
+            target.rotation = Quaternion.AngleAxis(euler.y, Vector3.up);
+        }
+    }
+
+    public static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360F)
+            angle += 360F;
+        if (angle > 360F)
+            angle -= 360F;
+        return Mathf.Clamp(angle, min, max);
+    }
+}
---
+++ PuzPlat Greybox/Assets/Script/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float jumpSpeed = 6.0f;
+    public float gravity = 20.0f;
+
+    public CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(inputH, 0, inputV);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
