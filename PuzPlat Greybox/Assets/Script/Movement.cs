c248cfdaf3adbcbba30b3f789318aff7b17842c9
Unknown <azzhayman@gmail.com>
2018-08-08 15:57:52 +1000

    Started first prototype

---
+++ PuzPlat Greybox/Assets/Script/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 5;
+
+
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+        {
+
+        }
+    }
+
+
+
+
+
+}
