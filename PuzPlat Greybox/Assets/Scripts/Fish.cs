0f455ede0576c1d35ef58addf5e61dcc33a97675
Unknown <azzhayman@gmail.com>
2018-08-27 13:31:20 +1000

    Finished fixing fish min distance

--- PuzPlat Greybox/Assets/Scripts/Fish.cs
+++ PuzPlat Greybox/Assets/Scripts/Fish.cs
@@ -28,8 +28,13 @@ public class Fish : MonoBehaviour
     public float curSpeed;
     public float followSpeed;
     public float catchupSpeed;
+    public float curRotSpeed;
     public float rotationSpeed = 10f;
+    public float orbitRotationSpeed;
     public float distConstrain = 5f;
+
+
+    public float minDist = 5f;
 
     public bool targetPlayer = false;
 
@@ -40,15 +45,19 @@ public class Fish : MonoBehaviour
     [Space(5)]
     public Rigidbody rigid;
     public GameObject target;
-    public GameObject player;
     public GameObject sacTarget;
     public GameObject switchTarget;
     #endregion
 
-    #region Player Script Reference
+    #region Player References
     [Header("Player Script Reference")]
     [Space(5)]
     public Player playerScript;
+    public GameObject player;
+    public bool waitingForPlayer = true;
+
+    public LayerMask layerMask;
+
     #endregion
 
 
@@ -60,59 +69,32 @@ public class Fish : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        followSpeed = Random.Range(0.5f, 5f);
+        followSpeed = Random.Range(2.5f, 7.5f);
         catchupSpeed = followSpeed * 2.5f;
         target = this.gameObject;
         playerScript = player.GetComponent<Player>();
+        orbitRotationSpeed = followSpeed / minDist;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target == player)
+        if (target.CompareTag("Player"))
         {
-            targetPlayer = true;
-        }
 
-        else
-        {
-            targetPlayer = false;
-        }
-
-        if (target != null && target != this.gameObject)
-        {
             rigid.velocity = transform.forward * curSpeed;
 
             FishDistConstrain();
 
-            var targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            var targetRotation = Quaternion.LookRotation((target.transform.position)- transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, curRotSpeed * Time.deltaTime);
         }
 
-        //if (Input.GetKeyDown(KeyCode.J))
-        //{
-        //    if (target == player)
-        //    {
-        //        if (playerScript.nearSacrifice)
-        //        {
-        //            target = playerScript.nearSacObj;
-        //        }
-
-        //        if (playerScript.nearSwitch)
-        //        {
-        //            target = playerScript.nearSwitchObj;
-        //        }
-        //    }
-        //    if (target == switchTarget && playerScript.nearSwitch)
-        //    {
-        //        target = player;
-        //    }
-        //}
-
-        //else if (Input.GetKeyUp(KeyCode.J) && target == sacTarget && playerScript.nearSacrifice)
-        //{
-        //    target = player;
-        //}
+        if (target.CompareTag("Switch"))
+        {
+            return;
+        }
 
     }
 
@@ -121,29 +103,37 @@ public class Fish : MonoBehaviour
         float dist = Vector3.Distance(player.transform.position, this.transform.position);
         if (dist <= distConstrain)
         {
+            curRotSpeed = rotationSpeed;
             curSpeed = followSpeed;
         }
-        else if (dist > distConstrain)
+        if (dist > distConstrain)
         {
+            curRotSpeed = rotationSpeed;
             curSpeed = catchupSpeed;
+        }
+        else if (dist < minDist)
+        {
+            curRotSpeed = orbitRotationSpeed;
         }
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.name == "Player")
+        if (other.name == "Player" && waitingForPlayer)
         {
             if (!other.gameObject.GetComponent<Player>().fishList.Contains(this.gameObject))
             {
                 target = other.gameObject;
+                player = other.gameObject;
                 other.gameObject.GetComponent<Player>().fishList.Add(this.gameObject);
                 //this.gameObject.GetComponent<Collider>().enabled = false;
+                waitingForPlayer = false;
             }
 
         }
     }
-
+    
 
     private void OnDestroy()
     {
