7a61535ae07004efe6419e2ddfd72e207ad32f08
Unknown <azzhayman@gmail.com>
2018-09-12 11:54:12 +1000

    Platform movement script
    Yet to test

---
+++ PuzPlat Greybox/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject movePlat;
+
+    public int checkpointIndex;
+    public Transform[] checkpoint;
+    public Transform target;
+
+    public Vector3 platDestination;
+
+    public Rigidbody rigid;
+
+    public float speed = 2.5f;
+
+    public bool vertMove, horzMove;
+    public bool pingPong = false;
+    public bool loopBack = false;
+
+    void Start()
+    {
+        rigid = movePlat.GetComponent<Rigidbody>();
+
+        if (this.gameObject.name.Contains("Vertical"))
+        {
+            vertMove = true;
+        }
+
+        else if (this.gameObject.name.Contains("Horizontal"))
+        {
+            horzMove = true;
+        }
+
+        for (int i = 0; i < checkpoint.Length; i++)
+        {
+            checkpoint[i] = GameObject.Find("Checkpoint " + i).transform;
+        }
+
+
+    }
+
+    private void Update()
+    {
+
+        movePlat.transform.position = Vector3.Lerp(checkpoint[checkpointIndex].position, checkpoint[checkpointIndex + 1].position, speed);
+
+        if (pingPong)
+        {
+            loopBack = true;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Waypoint"))
+        {
+            if (pingPong)
+            {
+                if (!loopBack)
+                {
+                    if (checkpoint.Length > checkpointIndex + 1)
+                    {
+                        checkpointIndex++;
+                    }
+
+                    if (checkpoint.Length == checkpointIndex + 1)
+                    {
+                        loopBack = true;
+                    }
+                }
+
+                else if (loopBack)
+                {
+                    if (checkpointIndex + 1 > 0)
+                    {
+                        checkpointIndex--;
+                    }
+
+                    if (checkpointIndex == 0)
+                    {
+                        loopBack = true;
+                    }
+                }
+            }
+
+            else if (!pingPong)
+            {
+                if (checkpoint.Length > checkpointIndex + 1)
+                {
+                    checkpointIndex++;
+                }
+
+                if (checkpointIndex == checkpointIndex + 1)
+                {
+                    checkpointIndex = 0;
+                }
+            }
+        }
+    }
+
+}
